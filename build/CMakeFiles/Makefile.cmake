6775bff95c61406004edc52b404349018c1d35d9
Mohamed Hany <mohany@aucegypt.edu>
2025-03-28 23:42:10 +0200

    Compiled the new feature with make and g++

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,5 +43,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Calculator.dir/DependInfo.cmake"
+  "CMakeFiles/calculator.dir/DependInfo.cmake"
+  "CMakeFiles/calculator_test.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/calculator_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator_test.dir/test.cpp.o"
+  "CMakeFiles/calculator_test.dir/test.cpp.o.d"
+  "calculator_test"
+  "calculator_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
